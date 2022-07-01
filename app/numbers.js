f90eb6c3fb172063bc0954f5f8d88513a2f7fbb4
KarakulinAleksey <mail.kotbegemot@ya.ru>
2022-07-01 17:15:39 +0500

    numberMultiply

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,38 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    // console.log(num.toString(2));
+    // console.log('1000'[0]);
+    const strBit = num.toString(2);
+    // console.log(strBit);
+    return (num.toString(2)[bit]);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    // console.log(num.toString(2).padStart(8, '0'));
+    return num.toString(2).padStart(8, '0');
   },
 
   multiply: function(a, b) {
-
+    const arr = [...b.toString()];
+    console.log(arr);
+    
+    for (let i = arr.length; i >=0; i--){
+      console.log(arr[i]);
+      let summ;
+      if (arr[i]==='.'){
+        summ++;
+        return ;
+      }
+    
+    }
+    console.log('summ',summ);
+    const val = a * b;
+    return (Number(val.toFixed(5)));
+    
   }
 };
